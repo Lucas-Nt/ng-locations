5503430f3998d588b76b9c0531fcfe4b45c46fd6
Lucas-Nt <lntaopoulos@gmail.com>
2024-04-27 04:12:59 +0300

    add google maps

--- src/app/features/map/map.component.ts
+++ src/app/features/map/map.component.ts
@@ -1,12 +1,183 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
+
+const EUROPE_MIDPOINT_POSITION = {
+  lat: 54.526,
+  lng: 15.2551,
+};
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [CommonModule],
+  imports: [GoogleMapsModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MapComponent {}
+export class MapComponent {
+  readonly mapOptions: google.maps.MapOptions = {
+    center: { ...EUROPE_MIDPOINT_POSITION },
+    maxZoom: 15,
+    minZoom: 3,
+    styles: [
+      {
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#f5f5f5',
+          },
+        ],
+      },
+      {
+        elementType: 'labels.icon',
+        stylers: [
+          {
+            visibility: 'off',
+          },
+        ],
+      },
+      {
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#616161',
+          },
+        ],
+      },
+      {
+        elementType: 'labels.text.stroke',
+        stylers: [
+          {
+            color: '#f5f5f5',
+          },
+        ],
+      },
+      {
+        featureType: 'administrative.land_parcel',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#bdbdbd',
+          },
+        ],
+      },
+      {
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#eeeeee',
+          },
+        ],
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#757575',
+          },
+        ],
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#e5e5e5',
+          },
+        ],
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#9e9e9e',
+          },
+        ],
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#ffffff',
+          },
+        ],
+      },
+      {
+        featureType: 'road.arterial',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#757575',
+          },
+        ],
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#dadada',
+          },
+        ],
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#616161',
+          },
+        ],
+      },
+      {
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#9e9e9e',
+          },
+        ],
+      },
+      {
+        featureType: 'transit.line',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#e5e5e5',
+          },
+        ],
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#eeeeee',
+          },
+        ],
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [
+          {
+            color: '#c9c9c9',
+          },
+        ],
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            color: '#9e9e9e',
+          },
+        ],
+      },
+    ],
+  };
+}
